a60603afcda8ee4ccef9dd4bcfee6fe47dbbfb72
Sidney Nguyen <sidneynguyendev@gmail.com>
2017-03-28 01:47:24 -0700

    Changed how notes are rendered for melody

--- public/engines/clickEngine.js
+++ public/engines/clickEngine.js
@@ -1,9 +1,23 @@
-var melody = '';
+var melodyOutput = '';
+var melody = [];
 
 $('.note').click(function() {
-  melody += $(this).text() + ' ';
-  $('#melody-output').text(melody);
+  var note = {
+    letter: $(this).text(),
+    octave: $('#melody-octave').val(),
+    length: $('#melody-notelength').val()
+  };
+  melody.push(note);
+  renderMelody();
 });
+
+function renderMelody() {
+  $('#melody-output').empty();
+  for (var i = 0; i < melody.length; i++) {
+    $('#melody-output').append($('<div>').addClass('melody-note-container').attr('id', i + '-melody').addClass('melody-length-' + melody[i].length)
+        .append($('<p>').addClass('melody-letter').addClass('melody-octave-' + melody[i].octave).text(melody[i].letter)));
+  }
+}
 
 var chords = '';
 
